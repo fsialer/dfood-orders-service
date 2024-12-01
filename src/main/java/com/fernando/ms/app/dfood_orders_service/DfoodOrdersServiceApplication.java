5a36628e34136aed8cd42d696343630b7f1e45ed
Fernando <asialer05@hotmail.com>
2024-12-01 18:32:14 -0500

    Estructura base de services orders

---
+++ src/main/java/com/fernando/ms/app/dfood_orders_service/DfoodOrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.app.dfood_orders_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfoodOrdersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfoodOrdersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/app/dfood_orders_service/DfoodOrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.app.dfood_orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfoodOrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
